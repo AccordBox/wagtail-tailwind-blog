826ec1c9ebd89d29d0a65a27d2693b184e099ba5
MichaelYin <hgyinjian@gmail.com>
2017-06-23 16:09:47 +0800

    init commit

---
+++ wagtail_tuto/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for wagtail_tuto project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagtail_tuto.settings.dev")
+
+application = get_wsgi_application()
