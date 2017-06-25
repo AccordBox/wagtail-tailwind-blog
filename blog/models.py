070d8dea088475cc60063274234ec10b5d9b2e5d
MichaelYin <hgyinjian@gmail.com>
2017-06-25 17:07:48 +0800

    add category and tag support

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django import forms
 from django.db import models
 
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel, PageChooserPanel
+from wagtail.wagtailsnippets.models import register_snippet
 
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.tags import ClusterTaggableManager
+
+from taggit.models import TaggedItemBase, Tag as TaggitTag
 
 class BlogPage(Page):
     description = models.CharField(max_length=255, blank=True,)
@@ -18,8 +24,35 @@ class BlogPage(Page):
 
 class PostPage(Page):
     body = RichTextField(blank=True)
-
+    categories = ParentalManyToManyField('blog.BlogCategory', blank=True)
+    tags = ClusterTaggableManager(through='blog.BlogPageTag', blank=True)
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
+        FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
+        FieldPanel('tags'),
     ]
 
+@register_snippet
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=80)
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('slug'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey('PostPage', related_name='post_tags')
+
+@register_snippet
+class Tag(TaggitTag):
+    class Meta:
+        proxy = True
