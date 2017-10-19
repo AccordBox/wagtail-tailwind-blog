7aa46eb80b4e27aeb2d024a6cebc68ade8506feb
MichaelYin <hgyinjian@gmail.com>
2017-10-19 21:06:02 +0800

    integrate Disqus comment

--- blog/templatetags/blogapp_tags.py
+++ blog/templatetags/blogapp_tags.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import six
+
 from django.template import Library, loader
 from django.core.urlresolvers import resolve
 
@@ -52,3 +54,25 @@ def post_tags_list(context):
     post_tags = post.tags.all()
 
     return {'blog_page': blog_page, 'request': context['request'], 'post_tags': post_tags}
+
+@register.inclusion_tag('blog/comments/disqus.html', takes_context=True)
+def show_comments(context):
+    blog_page = context['blog_page']
+    post = context['post']
+    path = post_date_url(post, blog_page)
+
+    raw_url = context['request'].get_raw_uri()
+    parse_result = six.moves.urllib.parse.urlparse(raw_url)
+    abs_path = six.moves.urllib.parse.urlunparse([
+        parse_result.scheme,
+        parse_result.netloc,
+        path,
+        "",
+        "",
+        ""
+    ])
+
+    return {'disqus_url': abs_path,
+            'disqus_identifier': post.pk,
+            'request': context['request']}
+
