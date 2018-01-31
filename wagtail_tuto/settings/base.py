3b1891f963ddfeb800441580d1a7a8ea85a44536
MichaelYin <hgyinjian@gmail.com>
2018-01-31 11:22:12 +0800

    fix STATIC_ROOT

--- wagtail_tuto/settings/base.py
+++ wagtail_tuto/settings/base.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_pub')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
