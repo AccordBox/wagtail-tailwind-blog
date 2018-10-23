1ee3237163f8846afce47f19a7a7a8d91eeda404
MichaelYin <hgyinjian@gmail.com>
2018-10-23 17:23:37 +0800

    change app directory

--- manage.py
+++ manage.py
@@ -9,4 +9,7 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(current_path, "wagtail_tuto"))
+
     execute_from_command_line(sys.argv)
--- wagtail_tuto/wsgi.py
+++ wagtail_tuto/wsgi.py
@@ -1,17 +1,13 @@
-"""
-WSGI config for wagtail_tuto project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
-from __future__ import absolute_import, unicode_literals
-
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+# This allows easy placement of apps within the interior
+# djmyblog directory.
+app_path = os.path.abspath(os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), os.pardir))
+sys.path.append(os.path.join(app_path, 'wagtail_tuto'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagtail_tuto.settings.dev")
 
