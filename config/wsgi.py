b3cf42ad5bbb938845e4d8ac96f9b2b27e270c50
Michaelyin <hgyinjian@gmail.com>
2019-07-18 17:44:44 +0800

    move settings

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,6 +9,6 @@ app_path = os.path.abspath(os.path.join(
     os.path.dirname(os.path.abspath(__file__)), os.pardir))
 sys.path.append(os.path.join(app_path, 'wagtail_tuto'))
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagtail_tuto.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
 
 application = get_wsgi_application()
