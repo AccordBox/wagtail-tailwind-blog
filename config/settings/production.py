12a6405a3b501505f75a980c59899f34f9c81818
Mizuki Sato <mizki.sato.0025@gmail.com>
2020-07-04 15:14:48 +0900

    use environment variables for production

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,8 +1,16 @@
 from __future__ import absolute_import, unicode_literals
 
+import os
+
 from .base import *
 
 DEBUG = False
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 try:
     from .local import *
