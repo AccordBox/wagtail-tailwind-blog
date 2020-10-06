ef24ad600000a375f2229708ab4bee429642a9f1
Damascene <411046+damascene@users.noreply.github.com>
2020-10-06 09:35:06 +0300

    Update views.py
    Fixing RemovedInWagtail211Warning

--- wagtail_tuto/home/views.py
+++ wagtail_tuto/home/views.py
@@ -5,8 +5,9 @@ from django.shortcuts import render
 
 from home.models import CopyWritingSettings
 
+from wagtail.core.models import Site
 
 def home(request):
-    social_media_setting = CopyWritingSettings.for_site(request.site)
+    social_media_setting = CopyWritingSettings.for_site(Site.find_for_request(request))
     context = {'social_media_setting': social_media_setting}
     return render(request, 'home/home_page.html', context=context)
