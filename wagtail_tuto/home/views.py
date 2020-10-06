19fe40c5a6ed4398bae81e209988844badbf4634
Damascene <411046+damascene@users.noreply.github.com>
2020-10-06 09:51:41 +0300

    Update views.py
    Readability and PIP8 fixes

--- wagtail_tuto/home/views.py
+++ wagtail_tuto/home/views.py
@@ -2,12 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from wagtail.core.models import Site
 from home.models import CopyWritingSettings
 
-from wagtail.core.models import Site
 
 def home(request):
-    social_media_setting = CopyWritingSettings.for_site(Site.find_for_request(request))
+    site = Site.find_for_request(request)
+    social_media_setting = CopyWritingSettings.for_site(site)
     context = {'social_media_setting': social_media_setting}
     return render(request, 'home/home_page.html', context=context)
