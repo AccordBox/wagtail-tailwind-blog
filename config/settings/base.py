5db4eb4a7f3aac7646ee59a6c76ffd91020e1c8a
Damascene <411046+damascene@users.noreply.github.com>
2020-10-06 09:37:20 +0300

    Update base.py
    Removed wagtail.core.middleware.SiteMiddleware to fix RemovedInWagtail211Warning

--- config/settings/base.py
+++ config/settings/base.py
@@ -69,7 +69,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'wagtail.core.middleware.SiteMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 ]
 
